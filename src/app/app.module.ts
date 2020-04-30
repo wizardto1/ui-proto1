fb4193a0f82212a2d327b4cd5eb50c4ab0a1691b
wizardto1 <ph429@drexel.edu>
2020-04-30 13:00:43 -0400

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,12 +20,13 @@ import { Website2Component } from './website2/website2.component';
 import { GrayBarComponent } from './gray-bar/gray-bar.component';
 import { LanguageComponent } from './language/language.component';
 import { TopGrayBarComponent } from './body/top-gray-bar/top-gray-bar.component';
+import { Website3Component } from './website3/website3.component';
 const appRoutes: Routes = [
-   { path: '', redirectTo : '/home', pathMatch:'full' },{path:'home', component: HomeComponent },{path:'learning',component:LearningCenterComponent},{path:'video',component:VideoComponent},{path:'eval',component:HomeEvalComponent},{path:'transition',component:TransitionComponent},{path:'contact',component:ContactComponent},{path:'website1',component:Website1Component},{path:'website2',component:Website2Component},{path:'settings',component:SettingsComponent},{path:'language',component:LanguageComponent}];
+   { path: '', redirectTo : '/home', pathMatch:'full' },{path:'home', component: HomeComponent },{path:'learning',component:LearningCenterComponent},{path:'video',component:VideoComponent},{path:'eval',component:HomeEvalComponent},{path:'transition',component:TransitionComponent},{path:'contact',component:ContactComponent},{path:'website1',component:Website1Component},{path:'website2',component:Website2Component},{path:'settings',component:SettingsComponent},{path:'language',component:LanguageComponent},{path:'website3',component:LanguageComponent}];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(appRoutes) ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, TopBarComponent, BottomBarComponent, LearningCenterComponent, VideoComponent, HomeEvalComponent, TransitionComponent, ContactComponent, BodyComponent, Website1Component, SettingsComponent, Website2Component, GrayBarComponent, LanguageComponent, TopGrayBarComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, TopBarComponent, BottomBarComponent, LearningCenterComponent, VideoComponent, HomeEvalComponent, TransitionComponent, ContactComponent, BodyComponent, Website1Component, SettingsComponent, Website2Component, GrayBarComponent, LanguageComponent, TopGrayBarComponent, Website3Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
